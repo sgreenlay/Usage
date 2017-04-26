ab11dc7ab3bc664db260e4e2016513ca84bd4088
Scott Greenlay <sgreenlay@gmail.com>
2017-04-25 18:12:59 -0700

    Add usage totals

--- js/traffic.js
+++ js/traffic.js
@@ -12,34 +12,61 @@ function update_UsageTable(usage) {
     var header = table.insertRow(0);
 
     var cell = document.createElement("th");
-    cell.width = "40%";
+    cell.width = "25%";
     cell.style = "text-align: center;"
     cell.innerHTML = "IP Address";
     header.appendChild(cell);
 
     cell = document.createElement("th");
-    cell.width = "30%";
+    cell.width = "25%";
     cell.style = "text-align: center;"
     cell.innerHTML = "Upload (Mb)";
     header.appendChild(cell);
 
     cell = document.createElement("th");
-    cell.width = "30%";
+    cell.width = "25%";
     cell.style = "text-align: center;"
     cell.innerHTML = "Download (Mb)";
     header.appendChild(cell);
 
+    cell = document.createElement("th");
+    cell.width = "25%";
+    cell.style = "text-align: center;"
+    cell.innerHTML = "Total (Mb)";
+    header.appendChild(cell);
+
     var clients = usage.split(';');
+    clients.splice(-1,1);
+
     clients.forEach(function(client) {
         var row = table.insertRow();
 
         var info = client.split(':');
-        info.forEach(function(item) {
-            var cell = document.createElement("td");
-            cell.innerHTML = item;
-            cell.style = "text-align: center;"
-            row.appendChild(cell);
-        });
+
+        var ip = info[0];
+        var cell = document.createElement("td");
+        cell.innerHTML = ip;
+        cell.style = "text-align: center;"
+        row.appendChild(cell);
+
+        var upload = info[1];
+        cell = document.createElement("td");
+        cell.innerHTML = upload;
+        cell.style = "text-align: center;"
+        row.appendChild(cell);
+
+        var download = info[2];
+        cell = document.createElement("td");
+        cell.innerHTML = download;
+        cell.style = "text-align: center;"
+        row.appendChild(cell);
+
+        var total = parseFloat(info[1]) + parseFloat(info[2]);
+        total = total.toFixed(2);
+        cell = document.createElement("td");
+        cell.innerHTML = total;
+        cell.style = "text-align: center;"
+        row.appendChild(cell);
     }, this);
 
     value.appendChild(table);
